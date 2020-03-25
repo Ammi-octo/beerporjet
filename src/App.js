391d491b424979870b21e7083957851fd5f7f6d7
michel.amoussou <michel.amoussou@octo.com>
2020-03-25 17:02:50 +0100

    commit III

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-import BeerList from './BeerList.js';
+import BeerList from "./BeerList";
 
 function App() {
+  const [value, setValue] = useState(1);
+  const precedant = () => {
+    if (value > 1) {
+      setValue(value - 1);
+    }
+  };
+
   return (
     <div className="App">
-    <BeerList/>
+      <button onClick={() => setValue(value + 1)}>suivant</button>
+      <button onClick={precedant}>precedant</button>
+      <BeerList pageNumber={value} />
     </div>
   );
 }
--- src/BeerList.js
+++ src/BeerList.js
@@ -1,15 +1,16 @@
 import React, { useEffect,useState } from 'react'
 
-export default function BeerList(){
+export default function BeerList({pageNumber}){
     const [ beers, setBeers ] = useState([])
     useEffect(() => {
-        fetch(`https://api.punkapi.com/v2/beers`)
+        fetch(`https://api.punkapi.com/v2/beers?page=${pageNumber}`)
         .then(reply => reply.json())
         .then(setBeers)
-    },[])
+    },[pageNumber])
 
-    return (
+    return ( 
         <ul>
+        <p>{pageNumber}</p>
         {beers.map(beer=> (
             <li key={beer.id}>{beer.name}</li>))} 
         </ul>
